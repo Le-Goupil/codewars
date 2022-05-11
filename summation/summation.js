2f3017d7307013d3d8c9f802eeffcdf3d6a252d3
Le-Goupil <apmonteiropro@gmail.com>
2022-05-11 16:36:20 +0200

    [ADD] summation, init repo

---
+++ summation/summation.js
@@ -0,0 +1,8 @@
+var summation = (num) => {
+  return (num * (num + 1)) / 2;
+};
+
+// console.log(summation(1));
+// console.log(summation(3));
+// console.log(summation(9));
+// console.log(summation(17));
