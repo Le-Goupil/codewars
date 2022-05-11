fdf7afba41af24e8ffe0a3b1d1b7bcce5bb7bcd5
Le-Goupil <apmonteiropro@gmail.com>
2022-05-11 16:53:27 +0200

    [FIX] some test didn't pass, missunderstood the instructions, fix

--- cockroach/cockroach.js
+++ cockroach/cockroach.js
@@ -1,3 +1,7 @@
 function cockroachSpeed(s) {
-  return Math.ceil(s * 27, 7778);
+  return Math.floor(s * 27.777778);
 }
+
+// console.log(cockroachSpeed(1.08));
+// console.log(cockroachSpeed(1.09));
+// console.log(cockroachSpeed(0));
